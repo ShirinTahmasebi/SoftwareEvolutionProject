15817134fd9aa8664f45723e6499c881d9df209f
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2018-08-18 23:27:07 +0430

    Save file to ipfs and display it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
+import ipfs from './ipfs'
 
 import './css/oswald.css'
 import './css/open-sans.css'
@@ -13,8 +14,13 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
+      buffer: null,
+      ipfsHash: ''
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -43,46 +49,75 @@ class App extends Component {
      * state management library, but for convenience I've placed them here.
      */
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    // const contract = require('truffle-contract')
+    // const simpleStorage = contract(SimpleStorageContract)
+    // simpleStorage.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    // // Declaring this for later so we can chain functions on SimpleStorage.
+    // var simpleStorageInstance
 
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+    // // Get accounts.
+    // this.state.web3.eth.getAccounts((error, accounts) => {
+    //   simpleStorage.deployed().then((instance) => {
+    //     simpleStorageInstance = instance
 
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
+    //     // Stores a given value, 5 by default.
+    //     return simpleStorageInstance.set(5, {from: accounts[0]})
+    //   }).then((result) => {
+    //     // Get the value from the contract to prove it worked.
+    //     return simpleStorageInstance.get.call(accounts[0])
+    //   }).then((result) => {
+    //     // Update state with the result.
+    //     return this.setState({ storageValue: result.c[0] })
+    //   })
+    // })
   }
+
+  onChange(event){
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+
+    const that = this;
+    reader.onload = function(event) {
+      // The file's text will be printed here
+      that.setState({buffer: Buffer(reader.result)});
+      console.log(that.state.buffer);
+    };
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    ipfs.files.add(this.state.buffer,  (error, result) => {
+      if (error) {
+        console.error(`onSubmit ipfs.add error is ${error}`);
+        return;
+      }
+
+      this.setState({ipfsHash: result[0].hash});
+      console.log(`ipfs hash is ${this.state.ipfsHash}`);
+    });
+  }
+  
 
   render() {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">Applicatoin Store</a>
         </nav>
 
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <h1>Your Image</h1>
+              <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
+              <h1>Upload Your Image</h1>
+              <form onSubmit={this.onSubmit}>
+                <input type='file' onChange={this.onChange}/>
+                <input type='submit'/>
+              </form>
             </div>
           </div>
         </main>
