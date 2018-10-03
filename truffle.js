6817de9a977cbd8db12582d1aa9ec96caf7d7d57
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2018-10-03 09:33:34 +0330

    Bad Commit :(

--- truffle.js
+++ truffle.js
@@ -1,6 +1,17 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   networks: {
+  	ropsten: {
+  		provider: function() {
+  			const HDWalletProvider = require('truffle-hdwallet-provider');
+  			return new HDWalletProvider(
+  				'media actress spike general shoot uniform absorb album because senior obvious warfare',
+  				'https://ropsten.infura.io/v3/ef081d851dea4cdb8a897d5e970405d6'
+  			);
+  		}, 
+  		gasPrice: 25000000000,
+  		network_id: 3
+  	},
     development: {
       host: "127.0.0.1",
       port: 7545,
